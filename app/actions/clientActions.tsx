2b981a996ca852ce9cb10cc68a2ae39193094d98
Chien-hsueh Huang <chienhsueh.h@gmail.com>
2025-01-22 12:04:33 -0800

    Initial features

---
+++ app/actions/clientActions.tsx
@@ -0,0 +1,172 @@
+'use server';
+
+import ps from "ps-node";
+
+export async function InitClient() {
+  /*ps.lookup({ pid: 12416 }, function(err, resultList ) {
+    if (err) {
+        throw new Error( "Error: no process with given pid found!", err );
+    }
+ 
+    var process = resultList[ 0 ];
+ 
+    if( process ){
+        console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
+    }
+    else {
+        console.log( 'No such process found!' );
+    }
+  });*/
+
+
+  /*
+  The important arguments:
+  - remoting-auth-token,
+  - app-port,
+  */
+  
+  console.log("getting client info");
+  try{
+    return new Promise((resolve) => {ps.lookup({
+      command: 'LeagueClientUx.exe',
+      //arguments: '--debug',
+      }, function(err, resultList ) {
+      if (err || resultList.length === 0 ) {
+          throw new Error( "Error: no such process found!", err );
+      }
+      else{
+        const credentials = {clientPortNumber: "", clientAuthToken: ""};
+        resultList.forEach(function( process ){
+            if( process ){
+              //console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
+              process.arguments.forEach(function ( argument ){
+                if ( argument.startsWith("--app-port=")){
+                  const clientPortNumber = argument.slice(11);
+                  console.log( 'Port: %s', clientPortNumber);
+                  credentials.clientPortNumber = clientPortNumber;
+                }
+                if ( argument.startsWith("--remoting-auth-token=")){
+                  const clientAuthToken = argument.slice(22);
+                  console.log( 'Auth token: %s', clientAuthToken);
+                  credentials.clientAuthToken = clientAuthToken;
+                }
+              })
+            }
+        });
+        resolve(credentials);
+      }
+    });})
+  } catch (e) { 
+    console.error((e as Error).message);
+  }
+  return null;
+}
+
+export async function GetSummonerId(password: string | undefined, port: string | undefined) {
+  // let password = 'ThT9Oai10ZzD0Sn-OwWV-Q';
+  // let port = '57267';
+
+  const headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa('riot:' + password));
+  let summonerResponse;
+  try {
+    const response = await fetch('https://127.0.0.1:' + port + '/lol-summoner/v1/current-summoner', {
+      method: "GET",
+      headers: headers,
+    });
+    summonerResponse = await response.json();
+    
+  } catch (e) {
+    console.error((e as Error).message);
+  }
+  
+  console.log("Reponse: ", summonerResponse);
+  let summonerId = summonerResponse.summonerId;
+  console.log("summonerId: ", summonerId);
+  return summonerId;
+}
+
+export async function GetChampsOwned(password: string | undefined, port: string | undefined) {
+  const headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa('riot:' + password));
+  let ownedChampsResponse;
+  try {
+    const response = await fetch('https://127.0.0.1:' + port + '/lol-champions/v1/owned-champions-minimal', {
+      method: "GET",
+      headers: headers,
+    });
+    ownedChampsResponse = await response.json();
+  } catch (e) {
+    console.error((e as Error).message);
+  }
+
+  const ownedChamps: number[] = [];
+  ownedChampsResponse.map((champ:any)=> {
+    ownedChamps.push(champ.id);
+  })
+  console.log("Owned Champions IDs: ", ownedChamps);
+
+  return ownedChamps;
+  // let data = await SkinsOwned(password, port, summonerId, ownedChamps);
+  // console.log("SkinsOwned: ", data);
+    
+}
+
+export async function GetSkinsOwned(password: string | undefined, port: string | undefined, summonerId: string, champId: number) {
+  const headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa('riot:' + password));
+  
+  let skinsResponse;
+  try {
+    const response = await fetch('https://127.0.0.1:' + port + '/lol-champions/v1/inventories/' + summonerId + '/champions/'+ champId +'/skins', {
+      method: "GET",
+      headers: headers,
+    });
+    skinsResponse = await response.json();
+    //console.log("Reponse: %s", data);
+  } catch (e) {
+    console.error(champId, (e as Error).message, summonerId);
+    return;
+  }
+  const skinsOwned:string[][] = [];
+  skinsResponse.map((skin: any) =>{
+    if (skin.ownership.owned && skin.name){
+      const skinNameAndURL:string[] = [];
+      skinNameAndURL.push(skin.name);
+      skin.loadScreenPath && skinNameAndURL.push(skin.loadScreenPath.slice(28).toLowerCase());
+      skinsOwned.push(skinNameAndURL);
+    }
+  })
+  //console.log("skinsOwned: ", skinsOwned);
+  return skinsOwned;
+}
+// export async function SkinsOwned(password: string | undefined, port: string | undefined, summonerId: string, ownedChamps: number[]) {
+//   // let password = 'ThT9Oai10ZzD0Sn-OwWV-Q';
+//   // let port = '57267';
+//   // summonerId = '66559580' //Ch13n
+
+//   const headers = new Headers();
+//   headers.append('Authorization', 'Basic ' + btoa('riot:' + password));
+  
+//   let skinsResponse;
+//   try {
+//     const response = await fetch('https://127.0.0.1:' + port + '/lol-champions/v1/inventories/' + summonerId + '/skins-minimal/', {
+//       method: "GET",
+//       headers: headers,
+//     });
+//     skinsResponse = await response.json();
+//     //console.log("Reponse: %s", data);
+//   } catch (e) {
+//     console.error((e as Error).message);
+//     return;
+//   }
+//   const skinsOwned:string[] = [];
+//   skinsResponse.map((skin: any) =>{
+//     skin.ownership.owned && skin.name && skinsOwned.push(skin.name);
+//   })
+//   //console.log("skinsOwned: ", skinsOwned);
+//   return skinsOwned;
+// }
+
+
+
---
+++ app/page.tsx
@@ -0,0 +1,186 @@
+'use client';
+
+import { useState } from 'react';
+import Image from "next/image";
+import { InitClient, GetSummonerId, GetChampsOwned, GetSkinsOwned} from '@/app/actions/clientActions';
+
+//process.env.NODE_EXTRA_CA_CERTS="@/riotgames.pem";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
+// console.log("NODE ENV");
+// console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_EXTRA_CA_CERTS);
+// console.log(process.env.NODE_TLS_REJECT_UNAUTHORIZED);
+
+export default function Home() {
+  const [creds, setCreds] = useState(null);
+  const [summonerId, setSummonerId] = useState(null);
+  const [champsOwned, setChampsOwned] = useState<number[]>([]);
+  const [skinsOwned, setSkinsOwned] = useState<string[][][]>([]);
+  const [champsWithoutSkins, setChampsWithoutSkins] = useState<string[]>([]);
+
+  async function handleInitClient(){
+    console.log("Handling init client");
+    const data:any = await InitClient();
+    console.log("data returned from InitClient: ", data);
+    if (typeof localStorage !== "undefined"){
+      console.log("saving clientPortNumber");
+      localStorage.setItem("clientPortNumber", data.clientPortNumber);
+      console.log("saving clientAuthToken");
+      localStorage.setItem("clientAuthToken", data.clientAuthToken);}
+    setCreds(data);
+  }
+
+  async function getSkinsOwnedFromChampsOwned(){
+    let skinsOwned: string[][][] = []
+    if (summonerId && champsOwned){
+      for (const id of champsOwned) {
+        let data = await GetSkinsOwned(localStorage.getItem('clientAuthToken')||"", localStorage.getItem('clientPortNumber')||"", summonerId, id);
+        data && skinsOwned.push(data);
+      }
+      skinsOwned.sort();
+    }
+    setSkinsOwned(skinsOwned);
+  }
+
+  async function getChampsWithNoSkinsOwned(){
+    let champsWithNoSkinsOwned: string[] = []
+    if (summonerId && champsOwned){
+      for (const id of champsOwned) {
+        let data = await GetSkinsOwned(localStorage.getItem('clientAuthToken')||"", localStorage.getItem('clientPortNumber')||"", summonerId, id);
+        data && data.length == 1 && champsWithNoSkinsOwned.push(data[0][0])
+      }
+      champsWithNoSkinsOwned.sort();
+    }
+    setChampsWithoutSkins(champsWithNoSkinsOwned);
+  }
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <button onClick={handleInitClient}>
+          Retrieve League Client Info
+        </button>
+        <button onClick={() => {creds?(console.log(creds)):(console.log("no creds yet"))}}>
+          Console log credentials
+        </button>
+        <button onClick={async () => {setSummonerId(await GetSummonerId(localStorage.getItem('clientAuthToken')||"", localStorage.getItem('clientPortNumber')||""));}}>
+          Get Current Summoner
+        </button>
+        <button onClick={async () => {setChampsOwned(await GetChampsOwned(localStorage.getItem('clientAuthToken')||"", localStorage.getItem('clientPortNumber')||""));}}>
+          Get Champions Owned
+        </button>
+        <button onClick={getChampsWithNoSkinsOwned}>
+          Get Champions I Own But Don't Have Skins For
+        </button>
+        <button onClick={getSkinsOwnedFromChampsOwned}>
+          Get All Skins Owned
+        </button>
+        {champsWithoutSkins.length!=0 ?(
+          <div>
+          {champsWithoutSkins.map((champ) => <span>{champ}. </span>)}
+          </div>
+        ):<p>p</p>}
+        {skinsOwned.length!=0 ? (
+          skinsOwned.map( (champ) => 
+            (<div>
+            <h1>{champ[0][0]}</h1>
+            {/*champ.splice(0,1)*/}
+            {champ.length > 1 && champ.map((skin, index) => index > 0 && <span>{skin[0]}<img alt="Skin splash" src={"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets"+skin[1]} ></img></span>)}
+            </div>)
+        )
+        ):(<p>p</p>)}
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
